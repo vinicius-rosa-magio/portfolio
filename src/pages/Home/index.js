59a130afa66c8acd7e738c8808e25f06f3072517
devrosa <viniciusrosamagio@gmail.com>
2023-09-26 13:09:52 -0300

    Atualização do meu sobre

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,23 +10,27 @@ function Home() {
 
     return (
         <div className="sobre">
-            
+
             <div className="conteudo">
                 <p className="paragrafo">
-                    "Olá, meu nome é Vinicius Rosa Mágio e sou um desenvolvedor web e mobile apaixonado por tecnologia e inovação.
-                    Após me formar em Arquitetura e Urbanismo, percebi que minha verdadeira paixão estava na área de desenvolvimento e decidi fazer uma transição de carreira.
-                    Com habilidades técnicas sólidas em HTML5, CSS3, JavaScript, React Native, jQuery e Bootstrap, sou capaz de atender às demandas do mercado de desenvolvimento web e mobile.
-                    Além disso, estou sempre aprimorando minhas habilidades e atualmente estou estudando REACT para oferecer soluções mais completas e inovadoras para projetos desafiadores.Sou uma pessoa motivada e comprometida com meu trabalho.
-                    Sempre em busca de aprendizado e crescimento, adoro trabalhar em equipe para alcançar os objetivos do projeto. Estou pronto para enfrentar novos desafios e me adaptar a diferentes ambientes de trabalho.
-                    Se você está procurando um desenvolvedor web e mobile comprometido e apaixonado por tecnologia e inovação, entre em contato comigo para conversarmos sobre como posso ajudar sua empresa a atingir seus objetivos."
+                    "Olá, meu nome é Vinicius Rosa Mágio e sou um entusiasta de desenvolvimento web e
+                    mobile com grande interesse em tecnologia e inovação. Após me formar em
+                    Arquitetura e Urbanismo, descobri minha verdadeira paixão na área de desenvolvimento e
+                    estou atualmente estudando React. Estou em constante aprimoramento das minhas habilidades
+                    para oferecer soluções mais completas e inovadoras para projetos desafiadores. Sou uma pessoa
+                    motivada e comprometida com meu trabalho, sempre buscando oportunidades de aprendizado e crescimento.
+                    Adoro trabalhar em equipe para alcançar os objetivos do projeto e estou pronto para enfrentar novos
+                    desafios e me adaptar a diferentes ambientes de trabalho. Se você está procurando um colaborador
+                    apaixonado por tecnologia e inovação, entre em contato comigo para conversarmos sobre como posso
+                    contribuir para o sucesso da sua empresa."
                     <Link to="/sobre" className="link"> Veja mais...</Link>
                 </p>
                 <img src={require('../../assets/Eu/Making Of - Espaço By Francine - Etyla Mariely Fotografia (98).jpg')} alt="foto do vinicius" className="imagem" />
             </div>
 
             <HardSkill />
-            <SoftSkill/>
-            <Projetos/>
+            <SoftSkill />
+            <Projetos />
         </div>
 
 
